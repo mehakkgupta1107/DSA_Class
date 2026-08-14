b4348d89a49017e6ae05efb5417ce8e9f1f2e18e
Mehak Gupta <mehakgupta1167@gmail.com>
2026-08-14 09:49:49 +0530

    swapped

--- swap.cpp
+++ swap.cpp
@@ -1,20 +1,26 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int n,k;
-    int a[n];
-    cin>>n>>k;
-    for(int i=0;i<n;i++)
-        cin>>a[i];
+int main() {
+    int n, k;
 
-    for(int i=0;i<n;i=i+2*k){
-        for(j=0;j<k;j++){
-            swap(a[i+j],a[i+j+k]);
+    cin >> n >> k;
+
+    vector<int> a(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    for (int i = 0; i < n; i = i + 2 * k) {
+        for (int j = 0; j < k; j++) {
+            swap(a[i + j], a[i + j + k]);
         }
     }
 
-    for(i=0;i<n;i++)
-        cout<<a[i]<<" ";
+    for (int i = 0; i < n; i++) {
+        cout << a[i] << " ";
+    }
+
     return 0;
 }
